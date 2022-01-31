86e6ae6f7105926d0a7c9349b7bbb58bca208bfd
Redish101 <luojiayun1001@163.com>
2022-01-31 21:33:37 +0800

    update 1 bugs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyqi-v1",
-    version="1.0.3-beta1",
+    version="1.0.3-beta2",
     author="Redish101",
     author_email="jiayunluo@outlook.com",
     description="It can help you quicker install python",
